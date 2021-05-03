c5fe645b04ad7a550fb02b0e65c6f21654753fd9
celian-rib <celian.riboulet@gmail.com>
2021-05-03 11:00:27 +0200

    Hello world

--- src/sokoban/Sokoban.java
+++ src/sokoban/Sokoban.java
@@ -16,6 +16,7 @@ public class Sokoban {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
